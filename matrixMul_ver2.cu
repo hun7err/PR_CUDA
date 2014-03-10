cca954669c7421065131730c433f885416dddca1
hun7err <hun7err@gmail.com>
2014-03-10 19:33:51 +0100

    Dodany pomiar GFlop/s + inne
    inne = generowanie macierzy, obsługa błędów itp.

--- matrixMul_ver2.cu
+++ matrixMul_ver2.cu
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
 
 #include <cuda_runtime.h>
 
@@ -22,80 +23,163 @@
 	}
 }
 
+float totalTime = 0.0f;
+#define TEST_COUNT 300
+
 int performMultiBlockTest(dim3 block_size, int width)
 {
+	printf("Block size (%d,%d) matrix width %d\n", block_size.x, block_size.y, width);
+
 	cudaError_t error;
 
-	float A[] = {1.0f,2.0f,3.0f,4.0f,5.0f,6.0f,7.0f,8.0f,9.0f,10.0f,11.0f,12.0f,13.0f,14.0f,15.0f,16.0f};
-	float B[] = {17.0f,18.0f,19.0f,20.0f,21.0f,22.0f,23.0f,24.0f,25.0f,26.0f,27.0f,28.0f,29.0f,30.0f,31.0f,32.0f};
+	//float A[] = {1.0f,2.0f,3.0f,4.0f,5.0f,6.0f,7.0f,8.0f,9.0f,10.0f,11.0f,12.0f,13.0f,14.0f,15.0f,16.0f};
+	//float B[] = {17.0f,18.0f,19.0f,20.0f,21.0f,22.0f,23.0f,24.0f,25.0f,26.0f,27.0f,28.0f,29.0f,30.0f,31.0f,32.0f};
+	float *A = (float*)malloc(width*width*sizeof(float));
+	float *B = (float*)malloc(width*width*sizeof(float));
 
-	float *C = (float*)malloc(16*sizeof(float));
-	memset(C, 0.0f, 16*sizeof(float));
+	for(int i = 0; i < width; i++)
+	{
+		for(int j = 0; j < width; j++)
+		{
+			A[i*width+j] = (float)rand() / (float)RAND_MAX;
+			B[i*width+j] = (float)rand() / (float)RAND_MAX;
+		}
+	}
+
+	float *C = (float*)malloc(width*width*sizeof(float));
+	memset(C, 0, width*width*sizeof(float));
 
 	float *A_d, *B_d, *C_d;
 
-	error = cudaMalloc((void**)&A_d, sizeof(A));
+	error = cudaMalloc((void**)&A_d, width*width*sizeof(float));
 
 	if(error != cudaSuccess)
 	{
-		fprintf(stderr, "Could not allocate memory on the device for matrix A (line: %d)\n", __LINE__);
+		fprintf(stderr, "Could not allocate memory on the device for matrix A: %s (line: %d)\n", cudaGetErrorString(error), __LINE__);
 		return -1;
 	}
 
-	error = cudaMalloc((void**)&B_d, sizeof(B));
+	error = cudaMalloc((void**)&B_d, width*width*sizeof(float));
 
 	if(error != cudaSuccess)
 	{
-		fprintf(stderr, "Could not allocate memory on the device for matrix B (line: %d)\n", __LINE__);
+		fprintf(stderr, "Could not allocate memory on the device for matrix B: %s (line: %d)\n", cudaGetErrorString(error), __LINE__);
 		return -1;
 	}
 
-	error = cudaMalloc((void**)&C_d, 16*sizeof(float));
+	error = cudaMalloc((void**)&C_d, width*width*sizeof(float));
 
 	if(error != cudaSuccess)
 	{
-		fprintf(stderr, "Could not allocate memory on the device for matrix C (line: %d)\n", __LINE__);
+		fprintf(stderr, "Could not allocate memory on the device for matrix C: %s (line: %d)\n", cudaGetErrorString(error), __LINE__);
 		return -1;
 	}
 
-	error = cudaMemcpy(A_d, A, sizeof(A), cudaMemcpyHostToDevice);
+	error = cudaMemcpy(A_d, A, width*width*sizeof(float), cudaMemcpyHostToDevice);
 
 	if(error != cudaSuccess)
 	{
-		fprintf(stderr, "Could not copy data from host to device (line: %d)\n", __LINE__);
+		fprintf(stderr, "Could not copy data from host to device: %s (line: %d)\n", cudaGetErrorString(error), __LINE__);
 		return -1;
 	}
 
-	error = cudaMemcpy(B_d, B, sizeof(B), cudaMemcpyHostToDevice);
+	error = cudaMemcpy(B_d, B, width*width*sizeof(float), cudaMemcpyHostToDevice);
 
 	if(error != cudaSuccess)
 	{
-		fprintf(stderr, "Could not copy data from host to device (line: %d)\n", __LINE__);
+		fprintf(stderr, "Could not copy data from host to device: %s (line: %d)\n", cudaGetErrorString(error), __LINE__);
 		return -1;
 	}
 
-	matrixMulMultiBlock<<<dim3(ceil((float)width/(float)block_size.x), ceil((float)width/(float)block_size.y)),block_size>>>(C_d, A_d, B_d, 4);
+	cudaEvent_t start;
+    error = cudaEventCreate(&start);
 
-	error = cudaMemcpy(C, C_d, 16*sizeof(float), cudaMemcpyDeviceToHost);
+    if (error != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to create start event: %s (line: %d)\n", cudaGetErrorString(error), __LINE__);
+        return -1;
+    }
+
+    cudaEvent_t stop;
+    error = cudaEventCreate(&stop);
+
+    if (error != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to create stop event: %s (line: %d)\n", cudaGetErrorString(error), __LINE__);
+        return -1;
+    }
+
+	error = cudaEventRecord(start, NULL);
+
+	if(error != cudaSuccess)
+	{
+		fprintf(stderr, "Could not record start event: %s (line: %d)\n", cudaGetErrorString(error), __LINE__);
+		return -1;
+	}
+
+	for(int current_test = 0; current_test < TEST_COUNT; current_test++)
+	{
+		matrixMulMultiBlock<<<dim3((int)ceil((float)width/(float)block_size.x), (int)ceil((float)width/(float)block_size.y)),block_size>>>(C_d, A_d, B_d, 4);
+	}
+
+	error = cudaEventRecord(stop, NULL);
+
+	if(error != cudaSuccess)
+	{
+		fprintf(stderr, "Could not record stop event: %s (line: %d)\n", cudaGetErrorString(error), __LINE__);
+		return -1;
+	}
+
+	error = cudaEventSynchronize(stop);
+
+	if(error != cudaSuccess)
+	{
+		fprintf(stderr, "Could not synchronize with stop event: %s (line: %d)\n", cudaGetErrorString(error), __LINE__);
+        return -1;
+	}
+
+	error = cudaEventElapsedTime(&totalTime, start, stop);
+
+	if(error != cudaSuccess)
+	{
+		fprintf(stderr, "Could not calculate elapsed time: %s (line: %d)\n", cudaGetErrorString(error), __LINE__);
+        return -1;
+	}
+
+	float msecPerMatrixMul = totalTime / TEST_COUNT;
+    double flopsPerMatrixMul = 2.0 * (double)width * (double)width * (double)width;
+    double gigaFlops = (flopsPerMatrixMul * 1.0e-9f) / (msecPerMatrixMul / 1000.0f);
+
+	printf("Performance: %.2f GFlop/s\n", gigaFlops);
+
+	error = cudaMemcpy(C, C_d, width*width*sizeof(float), cudaMemcpyDeviceToHost);
 	
 	if(error != cudaSuccess)
 	{
-		fprintf(stderr, "Could not copy data from device to host (line: %d)\n", __LINE__);
+		fprintf(stderr, "Could not copy data from device to host: %s (line: %d)\n", cudaGetErrorString(error), __LINE__);
 		return -1;
 	}
+
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
 
 	cudaFree(C_d);
 	cudaFree(B_d);
 	cudaFree(A_d);
 
 	free(C);
- }
+	free(B);
+	free(A);
+
+	return 0;
+}
 
 void performMultiBlockTests(void)
 {
-	performMultiBlockTest(dim3(3,3), 4);
-	/*performMultiBlockTest(dim3(8,8), 10);
-	performMultiBlockTest(dim3(16,16), 10);
-	performMultiBlockTest(dim3(22,22), 10);
-	performMultiBlockTest(dim3(32,32), 10);*/
+	srand((unsigned int)time(NULL));
+	//performMultiBlockTest(dim3(3,3), 4);
+	performMultiBlockTest(dim3(8,8), 200); // 10?
+	performMultiBlockTest(dim3(16,16), 200);
+	performMultiBlockTest(dim3(22,22), 200);
+	performMultiBlockTest(dim3(32,32), 200);
 }
