3ae9f50d8474cb682b26ac684ccb84ff39b9d5d3
Krzysztof Marciniak <hun7err@gmail.com>
2014-03-22 10:53:43 +0100

    plik batchowy do odpalenia testow

---
+++ odpal_testy_i_zbierz_wyniki.bat
@@ -0,0 +1,20 @@
+echo "Pojedynczy blok watkow"
+echo "====================================="
+PR3 1 > jeden_blok.txt
+echo "====================================="
+echo "Wiele blokow"
+echo "====================================="
+PR3 2 > wiele_blokow.txt
+echo "====================================="
+echo "Wykorzystanie pamieci wspoldzielonej"
+echo "====================================="
+PR3 3 > pamiec_wspoldzielona.txt
+echo "====================================="
+echo "Zrownoleglenie obliczen i pob. danych"
+echo "====================================="
+PR3 4 > pamiec_wspoldzielona_prefetch.txt
+echo "====================================="
+echo "4. + zwiekszenie pracy per watek"
+echo "====================================="
+PR3 5 > pamiec_wspoldzielona_wiecej_pracy.txt
+echo "Testy zakonczone"
